26400d1f0063edfc8bd3bd88f9814475ed608205
apodja <aldi.podaj@clickservice.at>
2022-08-18 17:18:17 +0200

    migration from mui to material dashboard bootstrap

--- timeTracker/frontend/src/components/App.js
+++ timeTracker/frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component,Fragment } from 'react';
 import Stopwatch from "./Stopwatch";
 import { render } from "react-dom";
 import {
@@ -7,9 +7,10 @@ import {
   Route,
 } from "react-router-dom";
 import Dashboard from './Dashboard';
-import SignInSide from './SignIn';
 import Register from './Register'
 import Login from './Login';
+import Navbar from './layout/Navbar';
+import Header from './layout/Header';
 
 
 
@@ -21,15 +22,23 @@ export default class App extends Component {
   render() {
     return (
       <div>
-      
-      <Router>
-      <Routes>
-          <Route exact path="/" element={<Stopwatch/>}/>
-          <Route exact path="/dashboard" element={<Dashboard/>}/>
-          <Route exact path="/login" element={<Login/>}/>
-          <Route exact path="/register" element={<Register/>}/>
-        </Routes>
-      </Router>
+        <Router>
+            <Fragment>
+              <Navbar/>
+              
+              <main className="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+                <Header />
+                <div className="container">
+                  <Routes>
+                    <Route exact path="/" element={<Stopwatch/>}/>
+                    <Route exact path="/dashboard" element={<Dashboard/>}/>
+                    <Route exact path="/login" element={<Login/>}/>
+                    <Route exact path="/register" element={<Register/>}/>
+                  </Routes>
+                </div>
+              </main>
+            </Fragment>
+        </Router>
       </div>
     );
   }
