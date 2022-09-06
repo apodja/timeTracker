5436a106e57a64b8b23253e90320df29b89edb57
apodja <aldi.podaj@clickservice.at>
2022-09-06 09:10:16 +0200

    change dashboard

--- timeTracker/dashboard/views.py
+++ timeTracker/dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from data.models import Entry
+from data.models import Entry,Task
 from django.utils import timezone
 from django.db.models import Sum
 from django.contrib.auth.decorators import login_required
@@ -13,29 +13,30 @@ def dashboard(request):
     filter = request.GET.get('filter')
     if filter is not None :
         if filter == 'day':
-            entries = Entry.objects.filter(user=user , created_at__date= timezone.now())
+            tasks  = Task.objects.filter(assigned_users = user, created_at__date= timezone.now()).count()
             total = Entry.objects.filter(user=user , created_at__date= timezone.now()).aggregate(Sum('duration'))
             if total['duration__sum'] is not None:
                 time = str(datetime.timedelta(seconds = total['duration__sum']))
             else:
                 time= 0
-            context ={'data' : entries ,  'total': time}
+            context ={'tasks' : tasks ,  'total': time}
         elif filter == 'month':
-            entries = Entry.objects.filter(user = user , created_at__month = timezone.now().month)
+            tasks  = Task.objects.filter(assigned_users = user, created_at__month = timezone.now().month).count()
             total = Entry.objects.filter(user=user , created_at__month= timezone.now().month).aggregate(Sum('duration'))
             if total['duration__sum'] is not None:
                 time = str(datetime.timedelta(seconds = total['duration__sum']))
             else:
                 time= 0
-            context ={'data' : entries ,  'total': time}
+            context ={'tasks' : tasks ,  'total': time}
     else:
-        entries = Entry.objects.filter(user=user)
+        tasks  = Task.objects.filter(assigned_users = user).count()
+
         total = Entry.objects.filter(user=user).aggregate(Sum('duration'))
         if total['duration__sum'] is not None:
             time = str(datetime.timedelta(seconds = total['duration__sum']))
         else:
             time= 0
-        context ={'data' : entries ,  'total': time}
+        context ={ 'total': time, 'tasks': tasks}
 
     return render(request, 'dashboard/pages/index.html', context)
 
